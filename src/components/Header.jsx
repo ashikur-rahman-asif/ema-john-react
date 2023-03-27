49ba5fc4d42b98e00d91e486988a07c26a7384f5
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-28 00:43:06 +0600

    header part done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,17 @@ import "./Header.css";
 import logo from "../extra/images/Logo.svg";
 const Header = () => {
   return (
-    <div className="header">
-          <img src={logo} alt="" />
-          <a href="/order">Order</a>
-          <a href="/order-review">Order Review</a>
-          <a href="/manage-inventory">Manage-Inventory</a>
-          <a href="/login">Login</a>
-    </div>
+    <nav className="header">
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div className="ahref">
+      <a href="/order">Order</a>
+      <a href="/order-review">Order-Review</a>
+      <a href="/manage-inventory">Manage-Inventory</a>
+      <a href="/login">Login</a>
+      </div>
+    </nav>
   );
 };
 
