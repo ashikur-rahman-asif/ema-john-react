8cb29fce0693d0e19056b60ddcb5da18fd27bdb8
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-28 01:14:36 +0600

    fetch solved

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([])
     useEffect(() => {
-        fetch('../../../public/products.json')
+        fetch('products.json')
             .then(res => res.json())
-        .then(data=>console.log(data))
+        .then(data=>setProducts(data))
     },[])
     return (
         <div className='shop-container' >
             <div className="products-container">
-        <h2>Products coming here</h2>
+                <h2>Products coming here:{products.length}</h2>
             </div>
             <div className="cart-container">
                 <h4>Order summary</h4>
