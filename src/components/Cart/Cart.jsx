a199824f0d98ceb97dcced9514d1dc087ba85f7f
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-28 15:45:37 +0600

    add to cart peoblem fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ const Cart = ({cart}) => {
     return (
         <div>
             <h4>Order summary</h4>
-              <p>Selected Items: {cart}</p>
+              <p>Selected Items: {cart.length}</p>
         </div>
     );
 };
