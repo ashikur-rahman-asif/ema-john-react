63b70e337c32d78cde0845d9c61f7d1ab2b1a595
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-29 01:55:25 +0600

    save cart to local storage

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,34 +2,38 @@ import React, { useEffect, useState } from "react";
 import "./Shop.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../extra/utilities/fakedb";
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const[cart,setCart]=useState([])
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
-    const handleAddToCart = (product) => {
-      const newCart = [...cart, product]
-        setCart(newCart)
-    }
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    console.log(storedCart);
+  }, []);
+  const handleAddToCart = (product) => {
+    // console.log(product)
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product.id);
+  };
   return (
     <div className="shop-container">
       <div className="products-container">
         {products.map((product) => (
-            <Product
-                key={product.id}
-                product={product}
-                handleAddToCart={handleAddToCart}
-            >
-                </Product>
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
         ))}
       </div>
       <div className="cart-container">
-        <Cart cart={cart}
-        
-        ></Cart>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
