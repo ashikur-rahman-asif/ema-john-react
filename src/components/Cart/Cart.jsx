c773dc622ea853283733e5e04342f4482029a26c
asif247 <86229363+asif247@users.noreply.github.com>
2023-03-28 20:39:03 +0600

    totalShipping

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import './Cart.css'
-const Cart = ({cart}) => {
-    // const cart= props.cart
-    // console.log(cart)
-    let total = 0
-    for (const product of cart) {
-        total=total+product.price
-        // console.log(product.price)
-    }
-    return (
-        <div className="cart">
-            <h4>Order summary</h4>
-            <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping: { } </p>
-            <p>Tax: { }</p>
-            <h6>Grand Total: { }</h6>
-        </div>
-    );
+import React from "react";
+import "./Cart.css";
+const Cart = ({ cart }) => {
+  // const cart= props.cart
+  // console.log(cart)
+  let total = 0;
+  let totalShipping = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    totalShipping = totalShipping + product.shipping;
+    // console.log(product.price)
+  }
+  return (
+    <div className="cart">
+      <h4>Order summary</h4>
+      <p>Selected Items: {cart.length}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping: {totalShipping} </p>
+      <p>Tax: {}</p>
+      <h6>Grand Total: {}</h6>
+    </div>
+  );
 };
 
 export default Cart;
